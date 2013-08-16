947fa4687d9e3db6f275ba5c152c45b254c99c0a
fanweixiao <fanweixiao@gmail.com>
2013-08-16 11:59:22 +0800

    update

--- settings/startup.lua
+++ settings/startup.lua
@@ -6,5 +6,4 @@ run_once("xrandr --output LVDS1 --mode 1366x768");
 run_once("xrandr --output VGA1 --mode 1440x900 --left-of LVDS1");
 run_once("pidgin");
 run_once("dropboxd");
-run_once("ibus-daemon -drx");
 run_once("volumeicon");
